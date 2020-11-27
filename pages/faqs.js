98c2cd20947b25bf8d1108d249c7ff8626716803
Al Joseph <condinoaljoseph@gmail.com>
2020-11-27 17:57:52 +0800

    faqs page

---
+++ pages/faqs.js
@@ -0,0 +1,11 @@
+import Layout from "../components/layouts/Layout";
+
+export default function Blogs() {
+	return (
+		<Layout>
+			<h1 className="text-center text-heading text-4xl font-bold">
+				Faqs
+			</h1>
+		</Layout>
+	);
+}
