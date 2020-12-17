91220b12b918a405d475991b786e12c8f299dcce
Al Joseph <condinoaljoseph@gmail.com>
2020-12-17 11:27:14 +0800

    fix color

--- pages/signin.js
+++ pages/signin.js
@@ -15,7 +15,7 @@ export default function Signin() {
 						Or
 						<a
 							href="#"
-							className="font-medium text-indigo-600 hover:text-indigo-500"
+							className=" text-indigo-600 hover:text-indigo-500"
 						>
 							start your 14-day free trial
 						</a>
@@ -73,7 +73,7 @@ export default function Signin() {
 						<div className="text-sm">
 							<a
 								href="#"
-								className="font-medium text-indigo-600 hover:text-indigo-500"
+								className=" text-indigo-600 hover:text-indigo-500"
 							>
 								Forgot your password?
 							</a>
@@ -83,7 +83,7 @@ export default function Signin() {
 					<div>
 						<button
 							type="submit"
-							className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+							className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm  rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 						>
 							<span className="absolute left-0 inset-y-0 flex items-center pl-3">
 								<svg
