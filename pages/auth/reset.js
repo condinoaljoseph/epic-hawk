d9e3797b12b03e8c433e24fee5c76cd279d85755
Al Joseph <condinoaljoseph@gmail.com>
2020-12-22 10:51:55 +0800

    :rocket:

--- pages/auth/reset.js
+++ pages/auth/reset.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import axios from 'axios';
+import axios from '../../utils/axios/api';
 
 export default function Reset() {
 	const [message, setMessage] = useState('');
