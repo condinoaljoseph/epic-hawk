f24e8f465d319001b78c32dc8031b2d442056ae8
Al Joseph <condinoaljoseph@gmail.com>
2020-12-22 10:49:33 +0800

    removed head

--- components/layouts/Main.js
+++ components/layouts/Main.js
@@ -1,20 +1,7 @@
-import Head from "next/head";
-
 export default function Main({ children }) {
 	return (
-		<>
-			<Head>
-				<title>Epic Hawk</title>
-				<link
-					rel="shortcut icon"
-					href="https://cdn2.whatoplay.com/developers/no_image.png"
-				/>
-			</Head>
-			<main id="main-content" className="h-screen bg-background">
-				<div className="max-w-7xl mx-auto px-4 sm:px-6 p-8">
-					{children}
-				</div>
-			</main>
-		</>
+		<main id="main-content" className="min-h-screen bg-background">
+			<div className="max-w-7xl mx-auto px-4 sm:px-6 p-8">{children}</div>
+		</main>
 	);
 }
